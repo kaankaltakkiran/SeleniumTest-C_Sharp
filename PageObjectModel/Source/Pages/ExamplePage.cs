d808cead11302f6ddfc7dcae4009fffc8e92385e
kaankalttakiran <durdu.kaan.kaltakkiran@gmail.com>
2023-05-20 21:34:14 +0300

    Tesr Added

---
+++ PageObjectModel/Source/Pages/ExamplePage.cs
@@ -0,0 +1,82 @@
+﻿using AngleSharp.Dom;
+using OpenQA.Selenium;
+using PageObjectModel.Reports;
+using PageObjectModel.Source.Drivers;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PageObjectModel.Source.Pages
+{
+    public class ExamplePage:Driver
+    {
+        [FindsBy(How =How.Id,Using = "userName")]
+        private IWebElement _fullName;
+
+        [FindsBy(How=How.Id,Using = "submit")]
+        private IWebElement _sendbtn;
+
+        [FindsBy(How =How.Id,Using = "userEmail")]
+        private IWebElement _usermail;
+        [FindsBy(How = How.Id, Using = "currentAddress")]
+        private IWebElement _currentaddress;
+        [FindsBy(How = How.Id, Using = "permanentAddress")]
+        private IWebElement _parmentetadress;
+
+        [FindsBy(How=How.CssSelector,Using = ".show .textt")]
+        private IWebElement _checkboxdiv;
+
+        [FindsBy(How = How.CssSelector, Using = "[for='yesRadio']")]
+        private IWebElement _checkbox;
+
+        [FindsBy(How = How.Id, Using = "uploadFile")]
+        private IWebElement _dowlandbtn;
+
+        [FindsBy(How = How.CssSelector, Using = ".rct-icon.rct-icon-expand-all")]
+        private IWebElement _pluscheckbox;
+
+        [FindsBy(How = How.CssSelector, Using = "li:nth-of-type(2) > ol > li:nth-of-type(1) > ol > li:nth-of-type(1) > .rct-text > label > .rct-checkbox > .rct-icon.rct-icon-uncheck")]
+        private IWebElement _selectcheckbox;
+
+        public ExamplePage() {
+            //Sekme büyütme
+            _driver.Manage().Window.Maximize();
+            PageFactory.InitElements(_driver, this);
+        }
+
+        public void textBoxForm(string name, string email,string currentAddress,string permanentAddress )
+        {
+            _fullName.SendKeys(name);
+            _usermail.SendKeys(email);
+            _currentaddress.SendKeys(currentAddress);
+            _parmentetadress.SendKeys(permanentAddress);
+            ExtentReporting.LogInfo($"Write '{name}','{email}','{currentAddress}','{permanentAddress}' Writed");
+            Thread.Sleep(2000);
+            _sendbtn.Click();
+        }
+        public void checkbox()
+        {
+           // _checkboxdiv.Click();
+            Thread.Sleep(1000);
+            _checkbox.Click();
+            ExtentReporting.LogInfo("Checkbox Clickted");
+
+        }
+        public void download()
+        {
+            _dowlandbtn.SendKeys(@"C:\Users\kaan_\Desktop\pc.png");
+            ExtentReporting.LogInfo("Upload Started ");
+
+        }
+        public void newcheckbox()
+        {
+            _pluscheckbox.Click();
+            _selectcheckbox.Click();
+            ExtentReporting.LogInfo("Checkbox Clickted");
+        }
+    }
+}
---
+++ PageObjectModel/Tests/ExamplePageTests.cs
@@ -0,0 +1,113 @@
+﻿using AngleSharp.Io;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using PageObjectModel.Reports;
+using PageObjectModel.Source.Drivers;
+using PageObjectModel.Source.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace PageObjectModel.Tests
+{
+    public class ExamplePageTests:Driver
+    {
+        [Test]
+        //Textboxtaki değeri okuyup ona göre test işlemi
+        public void textboxtest()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+            //Form elaman yollama
+            page.textBoxForm("Kaan","kaan_fb_aslan@hotmail.com","izmir","karşıyaka");
+            //Beklenen Değer
+            string expectedtext = "Kaan";
+            //Okunan Değer
+            string actualtext=_driver.FindElement(By.Id("userName")).GetAttribute("value");
+            Assert.AreEqual(expectedtext, actualtext);
+            ExtentReporting.LogInfo("Test Successful");
+        }
+        [Test]
+        //Sitedeki başlığa göre test işlemi
+        public void titleVerify()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+            page.textBoxForm("Kaan", "kaan_fb_aslan@hotmail.com", "izmir", "karşıyaka");
+            string expectedttitle = "DEMOQAA";
+            Assert.AreEqual(expectedttitle,_driver.Title);
+            ExtentReporting.LogInfo("Test Successful");
+        }
+        [Test]
+        //Sitedeki url göre test işlemi
+        public void urlVerify()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+            page.textBoxForm("Kaan", "kaan_fb_aslan@hotmail.com", "izmir", "karşıyaka");
+            string expectedUrl = "https://demoqa.com/text-box";
+            Assert.AreEqual(expectedUrl,_driver.Url,"Url Not Match");
+            ExtentReporting.LogInfo("Test Successful");
+        }
+        [Test]
+        //Textboxtaki değeri okuyup ona göre test işlemi
+        public void textboxtestt()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/text-box");
+            //Form elaman yollama
+            page.textBoxForm("Kaan", "kaan_fb_aslan@hotmail.com", "izmir", "karşıyaka");
+            //Beklenen Değer
+            string expectedtext = "Kaan";
+            //Okunan Değer
+            string actualtext = _driver.FindElement(By.CssSelector("p#name")).Text;
+            Assert.AreEqual("Name:Kaan", actualtext);
+            ExtentReporting.LogInfo("Test Successful");
+        }
+        [Test]
+        //Checkbox kontrol
+        public void checkboxtest()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/radio-button");
+            page.checkbox();
+            //Checkbox kontrolü yapılamadı
+            IWebElement element = _driver.FindElement(By.CssSelector("div:nth-of-type(2) > .custom-control-label"));
+            ExtentReporting.LogInfo("Test Successful");
+        }
+        [Test]
+        //File Download
+        public void fileDownload()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/upload-download");
+            page.download();
+            ExtentReporting.LogInfo("Test Successful");
+        }
+        [Test]
+        //Checkbox kontrol değer okuma ile
+        public void checkboxTestKaan()
+        {
+            ExtentReporting.LogInfo("Starting test ");
+            ExamplePage page = new ExamplePage();
+            _driver.Navigate().GoToUrl("https://demoqa.com/checkbox");
+            page.newcheckbox();
+            //Beklenen Değer
+            string expectedtext = "react";
+            //Okunan Değer
+            string actualtext = _driver.FindElement(By.CssSelector("div#result > .text-success")).Text;
+            Assert.AreEqual(expectedtext, actualtext);
+            ExtentReporting.LogInfo("Test Successful");
+        }
+    }
+}
